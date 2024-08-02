b1a6554ef1dc5472f91537dd9ee1049e77b3e820
santiagoperalta <santiago.peralta.3@hotmail.com>
2024-08-02 15:16:28 -0300

    Tercer commit: Agregado JS para validacion de credenciales locales

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const UsuariosEmpleados = [{id: 1 ,usuario: "Javier", clave: "1234"}, {id: 2 ,usuario: "Ezequiel", clave: "abcd"}];
+const Administrador = {usuario: "admin", clave: "admin"};
+
 document.getElementById("ingresoEmpleados").addEventListener("click", () => {
     let formularioIngresoAdmin = document.getElementById("formularioEmpleados");
     formularioIngresoAdmin.style.display = "flex"; 
@@ -19,4 +22,34 @@ document.getElementsByClassName("botonExit")[1].addEventListener("click", () => 
     formularioIngresoAdmin.style.display = "none"; 
 })
 
+function comprobacionCredenciales(usuarioWeb, claveWeb){
+    let validacion = false;
 
+    for( empleado of UsuariosEmpleados ){
+        if(usuarioWeb === empleado.usuario && claveWeb === empleado.clave){
+          validacion = true;   
+        }
+    }
+
+    return validacion
+}
+
+function redireccionWeb(pagWeb, validacion){
+    if( validacion ){
+        window.location.href = "../pages/admin"
+    }
+}
+
+document.getElementById("submitAdmin").addEventListener("click", () => {    
+
+    let usuarioAdminWeb = document.getElementById("nombreUsuarioAdmin").value;
+    let claveAdminWeb = document.getElementById("claveUsuarioAdmin").value;
+
+    document.getElementById("nombreUsuarioAdmin").value = "";
+    document.getElementById("claveUsuarioAdmin").value = "";
+
+    const validacion = comprobacionCredenciales(usuarioAdminWeb,claveAdminWeb);
+
+    console.log(validacion);
+    
+});
