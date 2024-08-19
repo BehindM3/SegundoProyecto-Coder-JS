575e1b6d57cdd169b370f75d9e17fd0761cd84dc
santiagoperalta <santiago.peralta.3@hotmail.com>
2024-08-19 17:29:39 -0300

    Septimo Commit: Logica Log-In

--- js/main.js
+++ js/main.js
@@ -1,22 +1,39 @@
-const UsuariosEmpleados = [{id: 1 ,usuario: "Javier", clave: "1234"}, {id: 2 ,usuario: "Ezequiel", clave: "abcd"}, {id: 3 ,usuario: "Lisandro", clave: "a1b2"}, {id: 4 ,usuario: "Luna", clave: "2001abc"}];
-const Administrador = {usuario: "admin", clave: "admin", };
-const Empleados = [{id: 1 ,usuario: "Javier", area: "Ingeniero"}, {id: 2 ,usuario: "Ezequiel", area: "RRHH"}, {id: 3 ,usuario: "Lisandro", area: "Gerente"}, {id: 4 ,usuario: "Luna", area: "Administrativo"}]
+const Empleados = [
+    {id: 1 ,usuario: "Javier", area: "Ingeniero"},
+    {id: 2 ,usuario: "Santiago", area: "RRHH"},
+    {id: 3 ,usuario: "Lisandro", area: "Gerente"},
+    {id: 4 ,usuario: "Luna", area: "Administrativo"}
+]
 
+const UsuariosEmpleados = [
+    {id: 1 ,usuario: "Javier", clave: "1234"},
+    {id: 2 ,usuario: "Santiago", clave: "abcd"},
+    {id: 3 ,usuario: "Lisandro", clave: "a1b2"},
+    {id: 4 ,usuario: "Luna", clave: "2001abc"}
+]
+
+const Administrador = {usuario: "admin", clave: "admin" };
+
+//Recargamos en el Local-Storage todas las  credenciales de empleados 
 Empleados.forEach(empleado => {
-    localStorage.setItem(empleado.usuario, empleado.area);
+    localStorage.setItem(empleado.usuario, JSON.stringify(empleado));
 });
 
+//Hacemos aparecer y desaparecer los menu de Log-In para empleados 
 document.getElementById("ingresoEmpleados").addEventListener("click", () => {
-    let formularioIngresoAdmin = document.getElementById("formularioEmpleados");
-    formularioIngresoAdmin.style.display = "flex"; 
+    let formularioIngresoEmpleado = document.getElementById("formularioEmpleados");
+    formularioIngresoEmpleado.style.display = "flex"; 
 })
 
 document.getElementsByClassName("botonExit")[0].addEventListener("click", () => {
-    let formularioIngresoAdmin = document.getElementById("formularioEmpleados");
-    formularioIngresoAdmin.style.display = "none"; 
+    let formularioIngresoEmpleado = document.getElementById("formularioEmpleados");
+    let denegacion = document.querySelector(".msjCredencialesDenegadas");
+    
+    formularioIngresoEmpleado.style.display = "none";
+    denegacion.style.display = "none"; 
 })
 
-
+//Hacemos aparecer y desaparecer los menu de Log-In para el admin 
 document.getElementById("ingresoAdmin").addEventListener("click", () => {
     let formularioIngresoAdmin = document.getElementById("formularioAdmin");
     formularioIngresoAdmin.style.display = "flex"; 
@@ -24,37 +41,64 @@ document.getElementById("ingresoAdmin").addEventListener("click", () => {
 
 document.getElementsByClassName("botonExit")[1].addEventListener("click", () => {
     let formularioIngresoAdmin = document.getElementById("formularioAdmin");
+    let denegacion = document.querySelector(".msjCredencialesDenegadas");
+    
+    denegacion.style.display = "none"
     formularioIngresoAdmin.style.display = "none"; 
+
 })
 
+//Comprobacion de credenciales
 function comprobacionCredenciales(usuarioWeb, claveWeb){
-    let validacion = false;
+    let usuarioIngresado = null;
 
-    for( empleado of UsuariosEmpleados ){
-        if(usuarioWeb === empleado.usuario && claveWeb === empleado.clave){
-          validacion = true;   
+    for( user of UsuariosEmpleados ){
+        if(usuarioWeb === user.usuario && claveWeb === user.clave){
+
+            Empleados.forEach( (empl) => {
+                if( user.id == empl.id){
+                    usuarioIngresado = empl;
+                }
+          })
+          
+          console.log(usuarioIngresado);
+        
         }
     }
 
-    return validacion
+    
+
+    return usuarioIngresado;
 }
 
-function redireccionWeb( validacion, usuario){
-    if( validacion ){
-        localStorage.setItem("empleadoSeleccionado", usuario);
-        window.location.href = "pages/empleado.html";
+//Redirigimos si las credenciales coinciden
+function redireccionWeb( usuarioValidado ){
+    
+    if( !usuarioValidado ){
+    
+        let denegacion = document.querySelector(".msjCredencialesDenegadas");
+        denegacion.style.display = "block"
+    
+    }else{
+    
+        localStorage.setItem("empleadoSeleccionado", JSON.stringify(usuarioValidado));
+        window.location.href = "./pages/empleado.html";
+    
     }
 }
 
-document.getElementById("submitEmpleados").addEventListener("click", () => {    
+document.getElementById("submitEmpleados").addEventListener("click", (ev) => {    
+
+    ev.preventDefault();
 
     let usuarioEmpleadoWeb = document.getElementById("nombreUsuarioEmpleado").value;
     let claveEmpleadoWeb = document.getElementById("claveUsuarioEmpleado").value;
 
-    const validacion = comprobacionCredenciales(usuarioEmpleadoWeb,claveEmpleadoWeb);
+    const usuarioValidado = comprobacionCredenciales(usuarioEmpleadoWeb,claveEmpleadoWeb);
 
-    redireccionWeb( validacion, usuarioEmpleadoWeb);
+    redireccionWeb( usuarioValidado);
 
     document.getElementById("nombreUsuarioEmpleado").value = "";
     document.getElementById("claveUsuarioEmpleado").value = "";
 });
+
